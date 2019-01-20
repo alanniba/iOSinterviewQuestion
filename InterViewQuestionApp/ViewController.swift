09d58b24091f02e9c0499b29f30d760d82fb56a1
haoyuan tan <957171869@qq.com>
2019-01-20 15:26:00 -0500

    Initial Commit

---
+++ InterViewQuestionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterViewQuestionApp
+//
+//  Created by haoyuan tan on 2019/1/20.
+//  Copyright © 2019 haoyuan tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
